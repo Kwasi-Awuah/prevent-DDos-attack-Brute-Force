c60a50dc5b04debc2bfe69db4aaf3e73f12d9a5a
Kwasi-Awuah <stawuah18@gmail.com>
2023-04-29 02:54:48 +0000

    Preventing DDos

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express') 
+const limitter = require('express-rate-limit')
+const app = express()
+const port = 3001
+
+
+// using it as a middleware protects the entire route in this file(or your project)
+app.use (limitter({
+    windowsMs : 5000,
+    max :5,
+    message: {
+        code: 429,
+        message: " Too many request"
+    }
+}))
+
+
+
+app.get('/rate' , (req,res)=> {
+    res.jsonp({"message" : "rate limter hot "})
+})
+
+app.get('/rate/api' , (req,res)=> {
+    res.jsonp({"message" : "rate api"})
+})
+ 
+app.get('/rate/v1' , (req,res)=> {
+    res.jsonp({"message" : "rate route v1 "})
+})
+
+// Protecting the register route making it available for only 2 
+const registerLimitter = limitter({
+    windowMs : 5000,
+    max : 2
+}) 
+app.post('/register',registerLimitter , (req,res)=> {
+    res.jsonp({"message" : "register page "})
+})
+
+// protecting the login route making it available for only 2 request 
+const loginLimitter = limitter({
+    windowMs : 5000,
+    max : 2
+})
+app.post('/login',loginLimitter , (req,res)=> {
+    res.jsonp({"message" : "Login has started "})
+})
+
+
+
+app.listen(port , console.log("server is runing !!!"))
